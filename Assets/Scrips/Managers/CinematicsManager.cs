1765d5731f24f47ee9684a4c6a5bf5617b351dfd
unknown <tomas.ardiles@davinci.edu.ar>
2026-06-12 23:12:02 -0300

    sistema de cinematicas y audio funcional

--- Assets/Scrips/Managers/CinematicsManager.cs
+++ Assets/Scrips/Managers/CinematicsManager.cs
@@ -1,15 +1,22 @@
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static GestorDeAudios;
 
 public class CinematicsManager : MonoBehaviour
 {
     public Animator reproduciendo;
     public int numero = 0;
 
+    public List<ListaDeAudios> AudioClipsDeCadaEscena;
+    public AudioSource parlante;
+    public int lista = 0;
+    public int clip = 0;
+    public AudioClip silencio;
+
+
     public CanvasGroup panelDeCinematicas;
-    //public List<Animator> cinematicas;
     public List<RuntimeAnimatorController> cinematicass;
 
     private void Start()
@@ -24,25 +31,33 @@ public class CinematicsManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E)) 
         { 
+            parlante.Stop();
             reproduciendo.SetTrigger("next");        
         }
         
     }
+    public void ReproducirSonido()
+    {
+        
+        parlante.PlayOneShot(AudioClipsDeCadaEscena[lista].clips[clip]);
+        clip++;
+    }
 
     public void CambioDeCinematica()
     {
+        lista++;
+        clip = 0;
         numero++;
         reproduciendo.runtimeAnimatorController = cinematicass[numero];
         GameManager.instance.Reanudar();
         panelDeCinematicas.gameObject.SetActive(false);
     }
-    //public void InicioDeCinematica()
-    //{
-    //    GameManager.instance.Pausar();
-    //    panelDeCinematicas.gameObject.SetActive(true);
-    //}
-
-
-
-
+}
+public class GestorDeAudios : MonoBehaviour
+{
+    [System.Serializable]
+    public class ListaDeAudios
+    {
+        public List<AudioClip> clips;
+    }    
 }
